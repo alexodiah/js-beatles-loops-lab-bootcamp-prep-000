3578bea8fb5b540b352a0e88f3c80a0158e21ab0
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-06-14 10:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function theBeatlesPlay(musicians, instrument) {
   var empty = []
     for(let m = 0; m < musicians.length; m++) {
@@ -17,10 +18,32 @@ var array = array;
 return(array)
 
 }
+=======
+// function theBeatlesPlay(musicians, instrument) {
+//   var empty = []
+//     for(let m = 0; m < musicians.length; m++) {
+//       empty.push(`${musicians[m]} plays ${instrument[m]}`)
+//       }
+//   return empty
+   
+// }
+  
+// function johnLennonFacts(array) {
+// var array = array;
+//   var i = 0;
+//   while (i < array.length) {
+//     array[i] = array[i] + "!!!";
+//     i++;
+//   }
+// return(array)
+
+// }
+>>>>>>> 8d9b2b8bb9019c5fea1590263f19e87ff73edf1e
 
 
 function iLoveTheBeatles(number)
 {
+<<<<<<< HEAD
   var num = number
   var fault = 'out of range'
   var x = 0;
@@ -40,3 +63,45 @@ return empty
 
 
 
+=======
+  var num = 0
+  var fault = 'out of range'
+  var x = 0;
+  var qoute = "I love the Beatles!"
+  function createVariable()
+  {
+            var empty = [];
+     
+            for(x = 0; x < number; x++)
+         { 
+             if(number < 15 )
+          {
+               do 
+         {
+           
+            empty.push(qoute)
+         
+         }                                            
+             while ( empty.length < num)
+            {
+       
+               }
+              } else if(number > 15)
+              
+              { 
+       
+                return qoute
+               
+              }else{
+                break;
+              }
+          
+              }
+         
+                console.log(empty)
+  
+             }
+  
+                  return createVariable()
+              }
+>>>>>>> 8d9b2b8bb9019c5fea1590263f19e87ff73edf1e
